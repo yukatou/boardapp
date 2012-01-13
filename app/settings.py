9ed0b5e9f616540ef35748310f1420989637bfc2
yukatou <yu.lets0226@gmail.com>
2012-01-14 03:59:14 +0900

    update

--- app/settings.py
+++ app/settings.py
@@ -1,4 +1,4 @@
 
-#DB_CONFIG = 'mysql://FLX_DB_USER:FLX_DB_PASS@FLX_DB_HOST:3306/FLX_DB_NAME?charset=utf8'
-DB_CONFIG = 'mysql://yukatou:yukatou@192.168.0.12:3306/boardapp?charset=utf8'
+DB_CONFIG = 'mysql://FLX_DB_USER:FLX_DB_PASS@FLX_DB_HOST:3306/FLX_DB_NAME?charset=utf8'
+#DB_CONFIG = 'mysql://yukatou:yukatou@192.168.0.12:3306/boardapp?charset=utf8'
 SECRET_KEY = 'boardapp_key'
